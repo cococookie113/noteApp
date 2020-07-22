25166d707b376716411b9e3a7792098cea9a707a
cococookie113 <cococookie113@gmail.com>
2020-07-23 00:50:37 +0900

    .ZZ

--- Components/Settings.js
+++ Components/Settings.js
@@ -7,24 +7,73 @@ import {
   ScrollView,
   TouchableOpacity,
 } from "react-native";
-//import { Updates } from "expo";
+import { Updates } from "expo";
+import { Restart } from "fiction-expo-restart";
 
 import Ionicons from "react-native-vector-icons/Ionicons";
 
 import Color from "./Color";
 
+//almost from https://digitalsynopsis.com/design/beautiful-color-palettes-combinations-schemes/
 let colorList = [
   "#30D0B7",
   "#5889F8",
   "#DB022C",
-  "#AD1C31",
-  "#55969D",
-  "#11A63A",
-  "#FFFF66",
-  "#FFE4E1",
-  "#8A2BE2",
-  "#407294",
-  "#FFF68F",
+  "#4b3832",
+  "#854442",
+  "#fff4e6",
+  "#3c2f2f",
+  "#be9b7b",
+  "#ee4035",
+  "#f37736",
+  "#fdf498",
+  "#7bc043",
+  "#0392cf",
+  "#4a4e4d",
+  "#0e9aa7",
+  "#3da4ab",
+  "#f6cd61",
+  "#fe8a71",
+  "#d11141",
+  "#00b159",
+  "#00aedb",
+  "#f37735",
+  "#ffc425",
+  "#fff6e9",
+  "#ffefd7",
+  "#fffef9",
+  "#e3f0ff",
+  "#d2e7ff",
+  "#2e003e",
+  "#3d2352",
+  "#3d1e6d",
+  "#8874a3",
+  "#e4dcf1",
+  "#343d46",
+  "#4f5b66",
+  "#65737e",
+  "#a7adba",
+  "#c0c5ce",
+  "#e3c9c9",
+  "#f4e7e7",
+  "#eedbdb",
+  "#cecbcb",
+  "#cbdadb",
+  "#051e3e",
+  "#251e3e",
+  "#451e3e",
+  "#651e3e",
+  "#851e3e",
+  "#ff77aa",
+  "#ff99cc",
+  "#ffbbee",
+  "#ff5588",
+  "#ff3377",
+  "#96ceb4",
+  "#ffeead",
+  "#ff6f69",
+  "#ffcc5c",
+  "#88d8b0",
 ];
 
 export default function Settings() {
@@ -54,7 +103,7 @@ export default function Settings() {
           </View>
           <View></View>
         </ScrollView>
-        {/* 
+
         <TouchableOpacity
           style={styles.restart}
           onPress={() => {
@@ -66,7 +115,7 @@ export default function Settings() {
             name="ios-checkmark"
             style={{ ...styles.restartText, color: global.color }}
           ></Ionicons>
-        </TouchableOpacity> */}
+        </TouchableOpacity>
       </SafeAreaView>
     </View>
   );
